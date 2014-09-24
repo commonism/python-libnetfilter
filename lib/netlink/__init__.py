a6a41a6bb5536cf276d4c495994b9e9f90269308
Markus Kötter <koetter@rrzn.uni-hannover.de>
2014-09-24 08:50:46 +0200

    nfnetlink - expose nl_ifhandle

--- lib/netlink/__init__.py
+++ lib/netlink/__init__.py
@@ -1,7 +1,41 @@
-from .libnfnetlink import _LP_nfnl_handle
+import ctypes
 
-__all__ = ['nfnl_handle']
+from .libnfnetlink import _LP_nfnl_handle, _LP_nlif_handle, libnfnl
+
+__all__ = ['nfnl_handle','nlif_handle']
 
 class nfnl_handle(_LP_nfnl_handle):
     _type_ = _LP_nfnl_handle
+
+
+class nlif_handle(_LP_nlif_handle):
+	_type_ = _LP_nlif_handle
+	_handle = None
+	_cnt = 0
+	@staticmethod
+	def open():
+		return ctypes.cast(libnfnl.nlif_open(), nlif_handle)
+
+	def close(self):
+		return libnfnl.nlif_close(self)
+
+	@staticmethod
+	def resolve(i):
+		if i == 0:
+			return ""
+		h = nlif_handle.open()
+		h.query()
+		r = h.index2name(i)
+		h.close()
+		return r
+
+	def query(self):
+		return libnfnl.nlif_query(self)
+
+	def index2name(self, index):
+		buf = ctypes.create_string_buffer(32)
+		r = libnfnl.nlif_index2name(self, index, buf)
+		if r < 0:
+			return None
+		return str(buf.value)
 
--- lib/netlink/libnfnetlink.py
+++ lib/netlink/libnfnetlink.py
@@ -1,5 +1,6 @@
 import ctypes
 import ctypes.util
+import struct
 
 libnfnl = ctypes.CDLL(ctypes.util.find_library("nfnetlink"))
 
@@ -14,11 +15,30 @@ class _nfnl_handle(ctypes.Structure):
 
 _LP_nfnl_handle = ctypes.POINTER(_nfnl_handle)
 
-class nlif_handle(ctypes.Structure):
+class _nlif_handle(ctypes.Structure):
 	pass
 
-_LP_nlif_handle = ctypes.POINTER(nlif_handle)
+_LP_nlif_handle = ctypes.POINTER(_nlif_handle)
 
+# struct nlif_handle *nlif_open(void);
+libnfnl.nlif_open.restype = _LP_nlif_handle
+libnfnl.nlif_open.argypes = []
+
+# void nlif_close(struct nlif_handle *orig);
+libnfnl.nlif_close.restype = None
+libnfnl.nlif_close.argypes = [_LP_nlif_handle]
+
+# int nlif_fd(struct nlif_handle *nlif_handle);
+# int nlif_query(struct nlif_handle *nlif_handle);
+libnfnl.nlif_open.restype = ctypes.c_int
+libnfnl.nlif_open.argypes = [_LP_nlif_handle]
+
+# int nlif_catch(struct nlif_handle *nlif_handle);
+# int nlif_index2name(struct nlif_handle *nlif_handle, unsigned int if_index, char *name);
+libnfnl.nlif_index2name.restype = ctypes.c_int
+libnfnl.nlif_index2name.argypes = [_LP_nlif_handle, ctypes.c_uint, ctypes.c_char_p]
+
+# int nlif_get_ifflags(const struct nlif_handle *h, unsigned int index, unsigned int *flags);
 
 class nlmsghdr(ctypes.Structure):
 	pass
